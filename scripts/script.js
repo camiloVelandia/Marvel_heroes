20469c564b5891ec5a370d6c0e7174c3cdb3dbb6
camilo <velandiacamilo356@gmail.com>
2020-02-27 21:07:01 -0500

    ajustes a el main

--- scripts/script.js
+++ scripts/script.js
@@ -1,14 +1,17 @@
 const $image = document.getElementById("image")
+const $name = document.getElementById("name")
+const $description = document.getElementById("description")
 const privateKey = '1c6597085b2ee3129c0154e5479cced5e9d570de',
        publicKey = 'a539ba370aebba43721a40ffbdc3af79'
 
 function renderImage(img){
 $image.setAttribute('src', img)
-
 }
-function renderData(info){
-
+function renderInfo(name, description){
+  $name.textContent = name
+  $description.textContent = description
 }
+
 
 const getConnection = () => {
   const ts = Date.now(),
@@ -17,13 +20,17 @@ const getConnection = () => {
   fetch(URL)
     .then(response => response.json())
     .then(data =>{
-      let ruta = data.data.results[18].thumbnail.path
-      console.log (ruta)
-      console.log (data.data.results)
-      renderImage(`${ruta}/portrait_xlarge.jpg`)
+      let ruta_base = data.data.results[4]
+      let ruta_image = ruta_base.thumbnail.path
+      //console.log (ruta)
+      console.log (ruta_base)
+      renderImage(`${ruta_image}/portrait_xlarge.jpg`)
+      renderInfo(ruta_base.name, ruta_base.description)
     
       })
       
 }
+
+
 getConnection()
 
