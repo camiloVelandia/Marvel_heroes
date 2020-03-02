e40a105ee91e666748313937f0a12d5c2e9c49c2
camilo <velandiacamilo356@gmail.com>
2020-03-01 20:25:29 -0500

    buscador terminado

--- scripts/script.js
+++ scripts/script.js
@@ -1,38 +1,24 @@
 const $search = document.getElementById("search")
+const $form = document.getElementById("form")
+const $button_search = document.getElementById("button_search")
 const $image = document.getElementById("image")
 const $name = document.getElementById("name")
 const $description = document.getElementById("description")
 const privateKey = '1c6597085b2ee3129c0154e5479cced5e9d570de',
        publicKey = 'a539ba370aebba43721a40ffbdc3af79'
 
+//get image from to API       
 function renderImage(img){
 $image.setAttribute('src', img)
 }
+
+//get image from to API   
 function renderInfo(name, description){
   $name.textContent = name
   $description.textContent = description
 }
 
-
-
-
-const getConnection = () => {
-  const ts = Date.now(),
-  hash = md5(ts + privateKey + publicKey),
-  URL = `https://gateway.marvel.com/v1/public/characters?ts=${ts}&apikey=${publicKey}&hash=${hash}`;
-  fetch(URL)
-    .then(response => response.json())
-    .then(data =>{
-      let ruta_base = data.data.results[4]
-      let ruta_image = ruta_base.thumbnail.path
-      console.log (data.data.results)
-      renderImage(`${ruta_image}/portrait_xlarge.jpg`)
-     renderInfo(ruta_base.name, ruta_base.description)
-      })
-      
-      
-}
-
+//connection to the API
 const searchHero = name => {
   const ts = Date.now(),
   hash = md5(ts + privateKey + publicKey),
@@ -44,27 +30,26 @@ const searchHero = name => {
       let ruta_base = data.data.results[0]
       let ruta_image = ruta_base.thumbnail.path
       renderImage(`${ruta_image}/portrait_xlarge.jpg`)
+      renderInfo(ruta_base.name, ruta_base.description)
       console.log(data.data.results)
     })
-    //.catch(e => console.log(e));
+    .catch(e => {
+      console.log('no encontrado')
+      renderInfo("404","not found")
+      $image.style.display= "none";
+    });
 
     
 };
-searchHero('black widow')
+searchHero('deadpool')
 
+$form.addEventListener('submit', (event)=>{
+  event.preventDefault()
+  const formData = new FormData(event.currentTarget)
+  //debugger
+  console.log(formData.get('hero-name'))
+  renderInfo(formData.get('hero-name'), "na")
+  searchHero(formData.get('hero-name'))
+})
 
-search.addEventListener('keyup', e =>{
- // debugger
-  if (e.KeyCode === 13) {
-    //debugger
-   searchHero(e.target.value.trim());
-   alert('personaje :'+e.target.value.trim());
- }else {
-
-    //alert('no recive enter');
-
- }
-
-});
-getConnection()
 
